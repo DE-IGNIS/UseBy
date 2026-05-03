c019e5cd6e6bf2c855e778024fc011fca48cc760
DE-IGNIS <chauhansudarsh@gmail.com>
2026-05-03 10:42:33 +0530

    init : implement feature files setup

---
+++ mobile/app/(tabs)/inventory.tsx
@@ -0,0 +1,11 @@
+import { View , Text} from "react-native";
+
+export default function Inventory() {
+  return (
+    <>
+      <View>
+        <Text>This is a inventory page</Text>
+      </View>
+    </>
+  );
+}
